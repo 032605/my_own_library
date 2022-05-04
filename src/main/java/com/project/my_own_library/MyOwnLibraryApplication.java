3008532b57b5c1efb10b175532776a2871c89f6e
032605 <032605@naver.com>
2022-05-04 17:22:38 +0900

    Initial commit

---
+++ src/main/java/com/project/my_own_library/MyOwnLibraryApplication.java
@@ -0,0 +1,14 @@
+package com.project.my_own_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyOwnLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyOwnLibraryApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/project/my_own_library/MyOwnLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.my_own_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyOwnLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
